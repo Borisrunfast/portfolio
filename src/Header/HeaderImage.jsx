2cfaf1a1a347c77c0bb859b9b76305afc79d5751
BorisSoaked <borisrunfast@gmail.com>
2023-04-04 20:40:50 +0200

    faster header imgs

--- src/Header/HeaderImage.jsx
+++ src/Header/HeaderImage.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
 import images from './imagesData'
 
-function TextImage(props) {
-    const {imageId} = props
-    let image = images[imageId].url
+function TextImage({ imageId }) {
 
-    return <img src={image} alt={images[imageId].alt} className='header-img    max-sm:w-[200px] max-sm:h-[200px]    max-lg:w-[350px] max-lg:h-[350px]'/>
+const show = [' hidden', ' hidden', ' hidden']
+
+switch (imageId) {
+    case 0: show[0] = ''
+    break
+    case 1: show[1] = ''
+    break
+    case 2: show[2] = ''
+}
+
+console.log(show)
+    return (
+        <>
+            <img src={images[0].url} alt={images[0].alt} className={'header-img absolute    max-sm:w-[200px] max-sm:h-[200px]    max-lg:w-[350px] max-lg:h-[350px]' + show[0]}/>
+            <img src={images[1].url} alt={images[1].alt} className={'header-img absolute    max-sm:w-[200px] max-sm:h-[200px]    max-lg:w-[350px] max-lg:h-[350px]' + show[1]}/>
+            <img src={images[2].url} alt={images[2].alt} className={'header-img absolute    max-sm:w-[200px] max-sm:h-[200px]    max-lg:w-[350px] max-lg:h-[350px]' + show[2]}/>
+        </>
+    )
 }
 
 export default TextImage
